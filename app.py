eb2f6960ace5daa5b32ccdb3e569019941cd6054
juanfranbrv <juanfranbrv@gmail.com>
2024-12-15 22:55:23 +0100

    Init commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Prueba de usos de Mixtral",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+st.header(f"Titulo de la app")
+    
